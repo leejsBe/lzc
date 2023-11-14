8d7e5168fff200fe794bfa587f7869cda582ce12
jslee <leejs1027@naturemobility.com>
2023-11-14 18:30:33 +0900

    프로젝트 생성

---
+++ src/main/java/kr/co/lzc/LzcApplication.java
@@ -0,0 +1,13 @@
+package kr.co.lzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LzcApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/co/lzc/LzcApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.lzc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
