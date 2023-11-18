ebab26243592882c8a790af177a3bd43f6dfec85
jslee <leejs1027@naturemobility.com>
2023-11-18 18:23:55 +0900

    전체 강연 등록 컨트롤러 구현

--- src/main/java/kr/co/lzc/domain/backOffice/controller/RegistrationLectureForBackOfficeController.java
+++ src/main/java/kr/co/lzc/domain/backOffice/controller/RegistrationLectureForBackOfficeController.java
@@ -1,6 +1,8 @@
 package kr.co.lzc.domain.backOffice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import kr.co.lzc.domain.backOffice.dto.registrationLecture.RegistrationLectureForBackOfficeReq;
+import kr.co.lzc.domain.backOffice.service.RegistrationLectureForBackOfficeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RegistrationLectureForBackOfficeController {
 
+  private final RegistrationLectureForBackOfficeService registrationLectureForBackOfficeService;
 
   /**
    * 강연 등록
    */
   @Operation(summary = "lecture registration", description = "강연 등록(back-office)")
   @PostMapping("/back-office/lecture")
-  public ResponseEntity<?> lectureRegistration() {
+  public ResponseEntity<Boolean> lectureRegistration(RegistrationLectureForBackOfficeReq registrationLectureForBackOfficeReq) {
+    registrationLectureForBackOfficeService.save(registrationLectureForBackOfficeReq);
     return ResponseEntity.ok(true);
   }
 
